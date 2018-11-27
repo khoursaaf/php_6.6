3812c0c9412d12e5362fb0b31372bc0e838d54f2
khoursaaf <khoursa@gmail.com>
2018-11-27 15:18:03 +0100

    correction

--- index.php
+++ index.php
@@ -1,29 +1,23 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Exercise</title>
-    </head>
-    <body>
-      <p>Créer un tableau months et l'initialiser avec les valeurs suivantes :</p>
-      <ol>
-        <li>janvier</li>
-        <li>février</li>
-        <li>mars</li>
-        <li>avril</li>
-        <li>mai</li>
-        <li>juin</li>
-        <li>juillet</li>
-        <li>aout</li>
-        <li>septembre</li>
-        <li>octobre</li>
-        <li>novembre</li>
-        <li>decembre</li>
-      </ol>
-      <?php
-      $months = ['janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre'];
-      for ($i = 0; $i < 11 ; $i++) {
-       ?>
-      <p><?php echo $months[$i]; } ?></p>
-    </body>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice</title>
+  </head>
+  <body>
+    <p>Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL existent et les afficher: index.php?building=12&room=101</p>
+    <a href="index.php?building=12&room=101">Afficher dans url!</a>
+    <?php
+    if (isset($_GET['building']) && isset($_GET['room'])) { ?>
+    <p>
+      <?= 'Les paramètres sont bien présent.'; ?>
+    </p>
+    <?php
+    }
+    else {
+    ?>
+    <p>
+      <?= 'Les paramètres ne sont pas présent'; }?>
+    </p>
+  </body>
 </html>
